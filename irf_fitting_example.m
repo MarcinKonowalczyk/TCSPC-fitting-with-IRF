52b72ac2ad320401d725d6f1d2561e730e0df269
Marcin Konowalczyk <marcin.konowalczyk@some.ox.ac.uk>
2020-08-19 19:04:22 +0100

    Cleanup and collate the optim functions into one

--- irf_fitting_example.m
+++ irf_fitting_example.m
@@ -15,41 +15,34 @@ a = gca; a.YScale = 'log';
 grid on; box on; axis square;
 
 %% Fit single exponential with a noise floor.
-
-f = @(p) optimfun_1(p,t,log10(y1),false);
-
-p0 = [10,1e4,10,5];
+f = @(p) optimfun_no_irf(p,t,log10(y1),[],false);
+p0 = [3, 3, 1e4, 10];
 pf = fminsearch(f,p0);
-[~,m] = optimfun_1(pf,t,log10(y1),true);
-% figure(1); clf;
-% plot(t,y1,t,m(t,p0),t,m(t,pf));
-% a = gca; a.YScale = 'log';
-% grid on; box on; axis square;
+[~,m] = optimfun_no_irf(pf,t,log10(y1),[],true);
 
 %% Fit double exponential with a noise floor
-w = ones(size(t)); % No weights
-f = @(p) optimfun_2(p,t,log10(y1),w,false);
+f = @(p) optimfun_no_irf(p,t,log10(y1),[],false);
 
-p0 = [3, 7e3,4.5, 1e4,0.3, 3];
+p0 = [3, 3, 7e3, 4.5, 1e4, 0.3];
 pf = fminsearch(f,p0);
-[~,m] = optimfun_2(pf,t,log10(y1),w,true);
+[~,m] = optimfun_no_irf(pf,t,log10(y1),[],true);
 
 %% Fit tripple exponential with a noise floor
-w = ones(size(t)); % No weights
-
 % Fit function
 plt = false; % Whether to plot over the course of fitting
-f = @(p) optimfun_3(p,t,log10(y1),w,plt);
+f = @(p) optimfun_no_irf(p,t,log10(y1),[],plt);
 
 % Parameter guess and constraints
-p0  = [3, 5e3, 4.6, 4e3, 0.2, 5e3, 0.01,  3];
-plb = [0, 1e2,   0, 1e2,   0, 1e2,    0,  0];
-pub = [5, 1e5,  10, 1e5,  10, 1e5,   10, 10];
+p0  = [3,  3, 5e3, 4.6, 4e3, 0.2, 5e3, 0.01];
+plb = [0,  0, 1e2,   0, 1e2,   0, 1e2,    0];
+pub = [5, 10, 1e5,  10, 1e5,  10, 1e5,   10];
 
 % Linear constraint tau1 > tau2 > tau 3
-A = [0 0 -1 0 1 0 0 0; 0 0  0 0 -1 0 1 0];
-b = [0;0];
+A = [0 0 0 -1 0 1 0 0; 0 0 0  0 0 -1 0 1];
+b = [0 0]';
 
 options = optimoptions('fmincon','Display','Iter');
 pf = fmincon(f,p0,A,b,[],[],plb,pub,[],options);
-[~,m] = optimfun_3(pf,t,log10(y1),w,true); % Plot and get the model function
+[~,m] = optimfun_no_irf(pf,t,log10(y1),[],true); % Plot and get the model function
+
+%%
---
+++ optimfun_no_irf.m
@@ -0,0 +1,78 @@
+function [diff,m] = optimfun_no_irf(p,time,data,weights,plot_flag)
+%% [diff,m] = optimfun_no_irf(p,t,data,w,plt)
+% Sum of n expnential impulse response functions with a noise floor.
+% 
+% 'p' if the vector of model parameters
+% p(1) - t0
+% p(2) - noise floor
+% p(3:4) - amplitude and time constant of 1st exp. component
+% p(5:6) - -//- of 2nd exp. component
+% p(7:8) - etc...
+
+if nargin < 5 || isempty(plot_flag)
+    plot_flag = false;
+end
+
+if nargin < 4 || isempty(weights)
+    weights = ones(size(time));
+end
+
+% Make sure parameters are all positive
+if any(p<0)
+    diff = Inf;
+    return
+end
+
+nf = @(t,p) p(2)*ones(size(t)); % Noise floor funciton
+
+%% Model function
+m = @(t,p) eir_model(t,p); % Alias the function so it can be passes 
+fit = log10(m(time,p)); % Fit to the data
+diff = (data-fit).*weights; % Difference between data and fit
+
+%% Plot
+if plot_flag
+    figure(1); clf;
+    s = subplot(4,1,1:3); hold on;
+    plot(time,data,time,fit,'k--');
+    
+    % Plot all the exp. impulse response functions
+    ne = (numel(p)-2)/2; % Number of eir components
+    for j = 1:ne
+        offset = 2*(j-1);
+        y = eir(time,p(1),p(3+offset),p(4+offset)) +  p(2)*ones(size(time));
+        plot(time,log10(y))
+    end
+    grid on; box on;
+    title(sprintf('TCSPC data fit, no IRF, %d eir functions',ne))
+    xlim([min(time),max(time)]);
+    ylabel('log10( Intensity )');
+    s.XTickLabel = [];
+    
+    s = subplot(4,1,4);
+    plot(time,diff,'k');
+    xlim([min(time),max(time)]);
+    s.YLim = [-1 1]*max(s.YLim);
+    grid on; box on;
+    title('Residuals');
+    xlabel('time / ps');
+    ylabel('\Delta log10( I )');
+    drawnow;
+end
+
+%% Root mean square residuals
+% Make sure no infs (-ve data) or nans (incomplete data)
+diff(isinf(diff) | isnan(diff)) = [];
+diff = sqrt(mean(diff.^2));
+end
+
+function y = eir_model(t,p)
+%%
+ne = (numel(p)-2)/2; % Number of exp. components
+y = 0;
+for j = 1:ne
+    offset = 2*(j-1);
+    y = y + eir(t,p(1),p(3+offset),p(4+offset));
+end
+y = y + p(2)*ones(size(t)); % Add noise floor
+end
